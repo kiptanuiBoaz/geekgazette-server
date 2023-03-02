9fb652d118dcf6a62741b1cbdf6c24a2bfeade41
kiptanuiBoaz <boaserem022@gmail.com>
2023-03-02 20:44:53 +0300

    added posts route

---
+++ routes/api/posts.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const postsController = require("../../controllers/postsController");
+const ROLES_LIST = require("../../config/roles");
+const verifyRoles = require("../../middleware/verifyRoles");
+
+
+//from employees controoller
+const { getPosts, deletePost, getPost, updatePost, createNewPost, } = postsController;
+
+router.route("/")
+    //get employees
+    .get(getPosts)
+
+    //update employee 
+    .put(updatePost)
+
+    //add employee
+    .post( createNewPost)
+
+    //delete employee
+    .delete( deletePost);
+
+router.route("/:id")
+    .get(getPost);
+
+
+module.exports = router;
