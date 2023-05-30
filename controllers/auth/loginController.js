4b81e37f273737e3b24e4338090da7c2be29981e
kiptanuiBoaz <boaserem022@gmail.com>
2023-05-30 12:15:43 +0300

    changed same site from lax to none in coockie options

--- controllers/auth/loginController.js
+++ controllers/auth/loginController.js
@@ -32,7 +32,7 @@ const handleLogin = async (req, res) => {
                 }
             },
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: "1d" }
+            { expiresIn: "1m" }
         )
 
         //create Refresh token
